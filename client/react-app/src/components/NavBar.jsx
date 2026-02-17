6b91c7ef9635c0c04f8c91d24becf80511e1560a
Manish Tamang <manishtamang0842@gmail.com>
2026-02-17 14:07:23 -0500

    update css

--- client/react-app/src/components/NavBar.jsx
+++ client/react-app/src/components/NavBar.jsx
@@ -8,14 +8,14 @@ function NavBar() {
         <div className="brand">
           <div className="brand-mark">🦴</div>
           <div className="brand-text">
-            <div className="brand-name">BoneLearn</div>
-            <div className="brand-sub">Fracture Learning Portal</div>
+            <div className="brand-name">FractureDetection</div>
+            <div className="brand-sub">AI Fracture Detection + Learning</div>
           </div>
         </div>
 
         <ul className="nav-links">
-          <li className="nav-link">Chapters</li>
-          <li className="nav-link">Quizzes</li>
+          <li className="nav-link">AI Tool</li>
+          <li className="nav-link">Learning Portal</li>
           <li className="nav-link">Flashcards</li>
         </ul>
 
