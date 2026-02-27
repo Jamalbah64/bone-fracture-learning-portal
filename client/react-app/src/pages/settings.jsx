1c287c6eab1b35900396f4471473ffec1e684670
Manish Tamang <manishtamang0842@gmail.com>
2026-02-27 01:21:43 -0500

    style update
    -fixed the spacing
    -centered everything
    -removes the three boxes

--- client/react-app/src/pages/settings.jsx
+++ client/react-app/src/pages/settings.jsx
@@ -1,11 +1,13 @@
 function Settings() {
   return (
     <section>
+       <div className="container">
       <h1>Settings</h1>
       <p>
         This page will expose advanced options and default model settings for
         the AI fracture detection workflow.
       </p>
+      </div>
     </section>
   );
 }
